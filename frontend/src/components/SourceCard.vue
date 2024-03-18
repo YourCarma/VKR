e694b7e188945f0a159ea82a63d6841b9fa3e88a
Your Carma <y0urcarma@yandex.ru>
2024-03-18 23:02:32 +0300

    SourceCardAdded

---
+++ frontend/src/components/SourceCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <div :class="`bg-${bg_color} max-w-xs rounded max-h-xs shadow-lg`">
+        <img class="w-full"
+            src="https://sun6-21.userapi.com/s/v1/ig2/NzeRweKn932nP043_plx1DWS5rQK9-hk9KOo1m5zfaP6C51WlHDjtEpG4qzlP1SBxevASPZedzd3FkOOidNviuZF.jpg?size=1077x1077&quality=96&crop=1,1,1077,1077&ava=1"
+            alt="Sunset in the mountains">
+        <div class="px-2 py-2 ">
+            <div class="font-bold text-sm mb-2 font-roboto">Повернутые на войне</div>
+            <div class="w-full flex-col">
+                <div class="w-full flex items-center">
+                    <BaseIcon :class="'text-white-100 w-6 h-6'" :name="'info'" />
+                    <a class="text-purple-700 text-center text-xs font-semibold font-roboto">https://t.me/voenacher</a>
+                </div>
+                <div class="w-full flex items-center">
+                    <BaseIcon :class="'text-white-100 w-6 h-6'" :name="'user'" />
+                    <p class="text-slate-700 text-center text-xs font-semibold font-roboto">1 245 523 подписчиков</p>
+                </div>
+
+            </div>
+            <p class="text-gray-700 text-xs">
+                Предложка и вопросы: @voenacher_zov_bot
+
+                По вопросам рекламы: @VoenacherAdvert_bot
+
+                @pnv_dozor_bot - передай информацию о ВРАГЕ!
+            </p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import BaseIcon from './BaseIcon.vue'
+export default {
+    components:{
+        BaseIcon
+    },
+
+    props: {
+        bg_color: String
+    }
+}
+</script>
+
+<style></style>
--- frontend/src/components/SourceManagementWindow.vue
+++ frontend/src/components/SourceManagementWindow.vue
@@ -1,32 +1,50 @@
 <template>
   <div class="flex w-full justify-center p-6 gap-4 duration-500">
-    <div class="fixed w-64 left-6 z-50">
+    <div class="fixed w-64 flex-col  left-6 z-50">
       <SidebarMain />
     </div>
     <div
-      class="flex flex-col min-h-[95vh] duration-500 justify-center ml-72 items-center w-full bg-frameBackground rounded-xl outline-dashed outline-[1px] outline-outlineColor"
-    >
-      <LoaderBig v-if="isLoading" class=""/>
-      
+      class="flex flex-col min-h-[95vh] duration-500 justify-center ml-72 items-center w-9/12 bg-frameBackground rounded-xl outline-dashed outline-[1px] outline-outlineColor">
+      <LoaderBig v-if="isLoading" class="" />
+
       <div v-else-if="isError" class="flex justify-center">
         <ErrorPage :status="error_info.code" :text="error_info.message" />
       </div>
-      <div v-else class="w-full mx-24 flex flex-col justify-center mb-10">
-        <div class="flex justify-center mb-4 pt-2">
-          <p
-            class="text-center text-4xl font-black font-mono text-activeText duration-500"
-          >
+      <div v-else class="w-full   justify-center ">
+        <div class="mx-2 mb-4 pt-2">
+          <p class="text-center text-4xl font-black font-mono text-activeText duration-500">
             Управление источниками
           </p>
         </div>
-        <div class="w-full mx-24  text-activeText flex justify-between"> 
-          <div> 
-            <div class="w-full outline-dashed outline-[3px] p-2 outline-outlineColor">
-              Количетсво источников:
+        <div class="w-full px-24">
+
+          <div
+            class=" text-activeText flex items-center justify-around ">
+            <div>
+              <div class="w-full  flex items-center  p-2 h-12 ">
+                Количеcтво источников:
+              </div>
+            </div>
+            <div class="w-36 h-12 bg-red-500 rounded-full flex items-center justify-center flex-col">
+              <span class="text-white text-xs font-bold">Пророссийских</span>
+              <span class="text-white text-lg font-bold">105</span>
+            </div>
+            <div class="w-36 h-12 bg-blue-500 rounded-full flex items-center justify-center flex-col">
+              <span class="text-white text-xs font-bold">Иных</span>
+              <span class="text-white text-lg font-bold">30</span>
+            </div>
+            <div class="w-36 h-12 bg-green-500 rounded-t-xl flex items-center justify-center flex-col hover:bg-green-700 duration-200">
+              <button class="text-white text-xl font-bold">Добавить</button>
             </div>
           </div>
-          <div class="w-full">
-              wafwaf
+          <div class="w-full mt-2 px-2 outline-dashed outline-[3px] outline-outlineColor flex justify-center overflow-y-auto h-full">
+            <div class="text-activeText w-50 grid grid-cols-3 gap-3 mr-2 max-h-[80vh]">
+              <SourceCard  class="my-2" :bg_color="'red-400'" v-for="el in 30"/>
+            </div>
+            <div class="outline-dashed outline-[1px] outline-outlineColor"></div>
+            <div class="text-activeText w-50 grid grid-cols-3 gap-3 ml-2 max-h-[80vh]">
+              <SourceCard  class="my-2" :bg_color="'blue-400'" v-for="el in 10"/>
+            </div>
           </div>
         </div>
       </div>
@@ -43,7 +61,7 @@ import BaseIcon from "./BaseIcon.vue";
 import ErrorPage from "@/components/ErrorPage.vue";
 import SidebarMain from "@/components/SidebarMain.vue";
 import LoaderBig from "./LoaderBig.vue";
-
+import SourceCard from './SourceCard.vue';
 export default {
   components: {
     BaseIcon,
@@ -52,6 +70,7 @@ export default {
     ErrorPage,
     SidebarMain,
     LoaderBig,
+    SourceCard
   },
   data() {
     return {
